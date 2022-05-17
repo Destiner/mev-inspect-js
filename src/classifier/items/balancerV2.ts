f5a6df1caaed8e07319d3d5c5d1e72d6771973d0
Timur Badretdinov <destinerx@gmail.com>
2022-05-17 13:51:24 +0400

    Fetch from/to from Balancer V2 swap clusters

--- src/classifier/items/balancerV2.ts
+++ src/classifier/items/balancerV2.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { AddressZero } from '@ethersproject/constants';
 import { Contract } from '@ethersproject/contracts';
 import { Provider } from '@ethersproject/providers';
 
@@ -24,40 +25,12 @@ function parseSwap(
   pool: Pool,
   event: ClassifiedEvent,
   transfers: Transfer[],
+  allEvents: ClassifiedEvent[],
 ): Swap | null {
-  const { values, transactionHash: hash, gasUsed, logIndex, address } = event;
+  const { transactionHash: hash, gasUsed, logIndex, address } = event;
+  const { assetIn, assetOut, amountIn, amountOut } = getSwapValues(event);
 
-  const assetIn = (values.tokenIn as string).toLowerCase();
-  const assetOut = (values.tokenOut as string).toLowerCase();
-  const amountIn = (values.amountIn as BigNumber).toBigInt();
-  const amountOut = (values.amountOut as BigNumber).toBigInt();
-
-  const swapTransfers = getSwapTransfers(logIndex, transfers);
-  if (!swapTransfers) {
-    return null;
-  }
-  const [transferIn, transferOut] = swapTransfers;
-  if (transferIn.event.address !== assetIn) {
-    return null;
-  }
-  if (transferOut.event.address !== assetOut) {
-    return null;
-  }
-  if (transferIn.value !== amountIn) {
-    return null;
-  }
-  if (transferOut.value !== amountOut) {
-    return null;
-  }
-  if (transferIn.to !== VAULT) {
-    return null;
-  }
-  if (transferOut.from !== VAULT) {
-    return null;
-  }
-  const from = transferIn.from;
-  const to = transferOut.to;
-
+  const { from, to } = getClusterInputOutput(logIndex, allEvents, transfers);
   return {
     contract: {
       address: pool.address,
@@ -90,7 +63,7 @@ function parseTransfer(event: ClassifiedEvent): Transfer {
   const token = (values.token as string).toLowerCase();
   const delta = (values.delta as BigNumber).toBigInt();
 
-  const from = delta > 0 ? VAULT : user
+  const from = delta > 0 ? VAULT : user;
   const to = delta > 0 ? user : VAULT;
   const value = delta > 0 ? delta : -delta;
 
@@ -110,37 +83,174 @@ function parseTransfer(event: ClassifiedEvent): Transfer {
   };
 }
 
-function getSwapTransfers(
+function getClusterInputOutput(
   logIndex: number,
+  allEvents: ClassifiedEvent[],
   transfers: Transfer[],
-): [Transfer, Transfer] | null {
-  const transferIn = transfers.find(
-    (transfer) => transfer.event.logIndex === logIndex + 1,
-  );
-  const transferOut = transfers.find(
-    (transfer) => transfer.event.logIndex === logIndex + 2,
-  );
-  if (!transferIn) {
-    return null;
+): {
+  from: string;
+  to: string;
+} {
+  function isVaultSwap(event: ClassifiedEvent): boolean {
+    return (
+      event.classifier.type === 'swap' &&
+      event.classifier.protocol === 'BalancerV2'
+    );
   }
-  if (!transferOut) {
-    return null;
+
+  const emptyInputOutput = {
+    from: AddressZero,
+    to: AddressZero,
+  };
+
+  const sortedEvents = [...allEvents];
+  sortedEvents.sort((a, b) => a.logIndex - b.logIndex);
+  // Go back from current event until not a Balancer V2 swap found
+  let startIndex = allEvents.findIndex((event) => event.logIndex === logIndex);
+  while (startIndex > 0 && isVaultSwap(allEvents[startIndex - 1])) {
+    startIndex--;
   }
-  return [transferIn, transferOut];
+  const startSwap = allEvents[startIndex];
+  // Go forward from current event until not a Balancer V2 swap found
+  let endIndex = allEvents.findIndex((event) => event.logIndex === logIndex);
+  while (
+    endIndex < allEvents.length - 1 &&
+    isVaultSwap(allEvents[endIndex + 1])
+  ) {
+    endIndex++;
+  }
+  const endSwap = allEvents[endIndex];
+  // Go forward from there until a swap found
+  let endTransferIndex = endIndex;
+  while (
+    endTransferIndex < allEvents.length - 1 &&
+    allEvents[endTransferIndex + 1].classifier.type === 'transfer'
+  ) {
+    endTransferIndex++;
+  }
+  // Make sure there is at least one transfer
+  if (endIndex === endTransferIndex) {
+    // Theoretically possible, but not economically viable
+    // Either way, w/o transfers, we can't deduct the sender
+    return emptyInputOutput;
+  }
+  const endTransfer = allEvents[endTransferIndex];
+  // Make sure all swaps in cluster form a valid chain
+  for (let i = startIndex; i < endIndex; i++) {
+    const swap = allEvents[i];
+    const nextSwap = allEvents[i + 1];
+    if (
+      swap.classifier.type !== 'swap' ||
+      nextSwap.classifier.type !== 'swap'
+    ) {
+      return emptyInputOutput;
+    }
+
+    const { assetOut: swapAssetOut, amountOut: swapAmountOut } =
+      getSwapValues(swap);
+    const { assetIn: nextSwapAssetIn, amountIn: nextSwapAmountIn } =
+      getSwapValues(nextSwap);
+
+    if (swapAssetOut !== nextSwapAssetIn) {
+      return emptyInputOutput;
+    }
+    if (swapAmountOut !== nextSwapAmountIn) {
+      return emptyInputOutput;
+    }
+  }
+  // Get cluster transfers, inflows, outflows
+  const clusterTransfers = transfers.filter(
+    (transfer) =>
+      transfer.event.logIndex > endSwap.logIndex &&
+      transfer.event.logIndex <= endTransfer.logIndex,
+  );
+  const clusterInflows = clusterTransfers.filter(
+    (transfer) => transfer.to === VAULT,
+  );
+  const clusterOutflows = clusterTransfers.filter(
+    (transfer) => transfer.from === VAULT,
+  );
+  // Get first inflow, last outflow
+  const firstInflow = clusterInflows[0];
+  const lastOutflow = clusterOutflows[clusterOutflows.length - 1];
+  // Make sure first inflow matches first swap in a cluster
+  if (clusterInflows.length > 0) {
+    const { assetIn, amountIn } = getSwapValues(startSwap);
+    if (
+      firstInflow.asset !== assetIn ||
+      (firstInflow.value !== amountIn && firstInflow.event.address !== VAULT) ||
+      firstInflow.to !== VAULT
+    ) {
+      return emptyInputOutput;
+    }
+  }
+  // Make sure last outflow matches last swap in a cluster
+  if (clusterOutflows.length > 0) {
+    const { assetOut, amountOut } = getSwapValues(endSwap);
+    if (
+      lastOutflow.asset !== assetOut ||
+      (lastOutflow.value !== amountOut &&
+        lastOutflow.event.address !== VAULT) ||
+      lastOutflow.from !== VAULT
+    ) {
+      return emptyInputOutput;
+    }
+  }
+  // Get input/output
+  const clusterFrom =
+    clusterInflows.length === 0
+      ? clusterOutflows.length === 0
+        ? AddressZero
+        : lastOutflow.to
+      : firstInflow.from;
+  const clusterTo =
+    clusterOutflows.length === 0
+      ? clusterInflows.length === 0
+        ? AddressZero
+        : firstInflow.from
+      : lastOutflow.to;
+  const from = logIndex === startSwap.logIndex ? clusterFrom : VAULT;
+  const to = logIndex === endSwap.logIndex ? clusterTo : VAULT;
+  return {
+    from,
+    to,
+  };
 }
 
-const CLASSIFIERS: Classifier[] = [{
-  type: 'swap',
-  name: 'Swap',
-  protocol: 'BalancerV2',
-  abi: vaultAbi,
-  parse: parseSwap,
-  fetchPool,
-}, {
-  type: 'transfer',
-  name: 'InternalBalanceChanged',
-  abi: vaultAbi,
-  parse: parseTransfer,
-}];
+function getSwapValues(swap: ClassifiedEvent): {
+  assetIn: string;
+  assetOut: string;
+  amountIn: bigint;
+  amountOut: bigint;
+} {
+  const assetIn = (swap.values.tokenIn as string).toLowerCase();
+  const assetOut = (swap.values.tokenOut as string).toLowerCase();
+  const amountIn = (swap.values.amountIn as BigNumber).toBigInt();
+  const amountOut = (swap.values.amountOut as BigNumber).toBigInt();
+
+  return {
+    assetIn,
+    assetOut,
+    amountIn,
+    amountOut,
+  };
+}
+
+const CLASSIFIERS: Classifier[] = [
+  {
+    type: 'swap',
+    name: 'Swap',
+    protocol: 'BalancerV2',
+    abi: vaultAbi,
+    parse: parseSwap,
+    fetchPool,
+  },
+  {
+    type: 'transfer',
+    name: 'InternalBalanceChanged',
+    abi: vaultAbi,
+    parse: parseTransfer,
+  },
+];
 
 export { fetchPool, CLASSIFIERS };
