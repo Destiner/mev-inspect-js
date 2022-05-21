15a0dc79d26cad3e0d8bf1b4910c191ea10832d1
Timur Badretdinov <destinerx@gmail.com>
2022-05-21 07:51:55 +0400

    Add Polygon examples

---
+++ examples/polygon.ts
@@ -0,0 +1,46 @@
+import { Inspector } from '../src/index.js';
+
+import getProvider from './utils.js';
+
+const CHAIN_ID = 137;
+
+interface Transaction {
+  hash: string;
+  label: string;
+}
+
+const transactions: Transaction[] = [
+  {
+    label: 'Balancer V2 internal arbitrage',
+    hash: '0xd8b484b99a0742f87a51f7fa656e0abdd3b38ae36074a896cbfcb9f794dfb706',
+  },
+  {
+    label: 'Balancer V2 X Sushiswap arbitrage',
+    hash: '0xde56117bfdf9fe034b9e6bdebfc113df28ae93e8cd26c2be872f0c251c256aeb',
+  },
+  {
+    label: 'Aave V2 liquidation',
+    hash: '0xb86d39f97dde86b792a00e21852399ec022ecf0c4de043b2bd6b53d3a5b2efe5',
+  },
+  {
+    label: 'Aave V3 liquidation',
+    hash: '0x2eae1162064c0560ed72d5bb7b375cee0e22886796a240533c9589fc77dec67b',
+  },
+];
+
+async function run(): Promise<void> {
+  const provider = getProvider(CHAIN_ID);
+  const inspector = new Inspector(CHAIN_ID, provider);
+  for (const transaction of transactions) {
+    const { hash, label } = transaction;
+    const txMev = await inspector.tx(hash);
+    console.log(
+      `Transaction { hash = ${hash}, label = ${label}, mev found = ${txMev.length} }`,
+    );
+    for (const mev of txMev) {
+      console.log(mev);
+    }
+  }
+}
+
+run();
