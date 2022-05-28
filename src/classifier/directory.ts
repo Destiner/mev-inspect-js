ced6e3037181ff763ff8dfa8f2a889de591b4fef
Timur Badretdinov <destinerx@gmail.com>
2022-05-28 15:09:51 +0400

    Add Arbitrum native asset

--- src/classifier/directory.ts
+++ src/classifier/directory.ts
@@ -9,7 +9,7 @@ type ChainId = typeof ETHEREUM | typeof POLYGON | typeof ARBITRUM;
 const nativeAsset: Record<ChainId, string> = {
   [ETHEREUM]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
   [POLYGON]: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
-  [ARBITRUM]: '',
+  [ARBITRUM]: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
 };
 
 const swapFactories: Record<ChainId, Record<SwapProtocol, string[]>> = {
