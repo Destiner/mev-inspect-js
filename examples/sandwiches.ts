e27aaee899d88ca739bccd3412012e6bd83490d1
Timur Badretdinov <destinerx@gmail.com>
2022-07-03 09:23:29 +0400

    Update sandwich example

--- examples/sandwiches.ts
+++ examples/sandwiches.ts
@@ -1,4 +1,4 @@
-import { Mev, Inspector, Sandwich, isSandwich } from '../src/index.js';
+import { Inspector, getSandwiches } from '../src/index.js';
 
 import getProvider from './utils.js';
 
@@ -10,31 +10,38 @@ interface Transaction {
 }
 
 const transactions: Transaction[] = [
-{
-  block: 14899935,
-  label: 'Uniswap V2 sandwich',
-}, {
-  block: 14898920,
-  label: 'Uniswap V2 sandwich',
-}, {
-  block: 14899831,
-  label: 'Uniswap V2 sandwich with non-ether profit asset',
-}, {
-  block: 14896835,
-  label: 'Uniswap V2 sandwich (large volume)',
-}, {
-  block: 14899113,
-  label: 'Sushiswap sandwich',
-}, {
-  block: 13699765,
-  label: 'Sandwich with partial frontswap buy',
-}, {
-  block: 14899088,
-  label: 'Sandwich with partial backswap sell',
-}, {
-  block: 14899837,
-  label: 'Uniswap V2 sandwich with backswap split',
-}
+  {
+    block: 14899935,
+    label: 'Uniswap V2 sandwich',
+  },
+  {
+    block: 14898920,
+    label: 'Uniswap V2 sandwich',
+  },
+  {
+    block: 14899831,
+    label: 'Uniswap V2 sandwich with non-ether profit asset',
+  },
+  {
+    block: 14896835,
+    label: 'Uniswap V2 sandwich (large volume)',
+  },
+  {
+    block: 14899113,
+    label: 'Sushiswap sandwich',
+  },
+  {
+    block: 13699765,
+    label: 'Sandwich with partial frontswap buy',
+  },
+  {
+    block: 14899088,
+    label: 'Sandwich with partial backswap sell',
+  },
+  {
+    block: 14899837,
+    label: 'Uniswap V2 sandwich with backswap split',
+  },
 ];
 
 async function run(): Promise<void> {
@@ -43,7 +50,7 @@ async function run(): Promise<void> {
   for (const transaction of transactions) {
     const { block, label } = transaction;
     const blockMev = await inspector.block(block);
-    const sandwichMev = blockMev.filter((mev: Mev): mev is Sandwich => isSandwich(mev))
+    const sandwichMev = getSandwiches(blockMev);
     console.log(
       `Block { block = ${block}, label = ${label}, mev found = ${sandwichMev.length} }`,
     );
